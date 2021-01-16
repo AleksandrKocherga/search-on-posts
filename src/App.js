8113b243d1546c7434c8490ac113ca3fe4875a11
AleksandrKocherga <senior.kocherga@gmail.com>
2021-01-16 14:40:15 +0300

    body styled fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import PostsList from './components/PostsList/PostsList';
 import SearchInput from './components/SearchInput/SearchInput';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+  padding: 0;
+  margin: 0;
+  background-image: linear-gradient(to left, #ffcc00  40%, #f9f586 60%);
+}
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <SearchInput />
       <PostsList />
     </>
